d9b1280e1f05327614ab4a6750d9ea2dcd17efab
Krystian Petek <krystianpetek2@gmail.com>
2022-04-27 15:58:17 +0200

    add first models and authentication

---
+++ VacationPlannerAPI/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VacationPlannerAPI.Models;
+
+public class Employee
+{
+    [Required, Display(Name = "ID")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "First name is required"), Display(Name = "First name")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Last name is required"), Display(Name = "Last name")]
+    public string LastName { get; set; }
+
+
+}
