8a90a1bd67f70025503e01688c9731d61b87871d
Krystian Petek <krystianpetek2@gmail.com>
2022-05-22 18:13:40 +0200

    add async login button

--- VacationPlannerWPFApp/Command/LoginCommand.cs
+++ VacationPlannerWPFApp/Command/LoginCommand.cs
@@ -11,37 +11,39 @@ using VacationPlannerWPFApp.ViewModels;
 
 namespace VacationPlannerWPFApp.Command
 {
-    public class LoginCommand : ICommand
+    public class LoginCommand : AsyncCommandBase
     {
-        public event EventHandler? CanExecuteChanged;
-
         private LoginViewModel viewModel;
 
-        public bool CanExecute(object? parameter)
-        {
-            return true;
-        }
         public LoginCommand(LoginViewModel viewModel)
         {
             this.viewModel = viewModel;
         }
 
-        public async void Execute(object? parameter)
+        protected override async Task ExecuteAsync(object? parameter)
         {
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Add("ApiKey", App.key);
+            await Login();
+        }
 
-                var data = new StringContent(JsonConvert.SerializeObject(new
-                {
-                    username = $"{viewModel.Login}",
-                    password = $"{viewModel.Password}"
-                }));
-                data.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-                var response = client.PostAsync("https://localhost:7020/api/user/login", data).Result;
-                
-                viewModel.Info = await response.Content.ReadAsStringAsync();
-            }
+        private async Task Login ()
+        {
+           await Task.Run(async () =>
+           {
+               using (HttpClient client = new HttpClient())
+               {
+                   client.DefaultRequestHeaders.Add("ApiKey", App.key);
+
+                   var data = new StringContent(JsonConvert.SerializeObject(new
+                   {
+                       username = $"{viewModel.Login}",
+                       password = $"{viewModel.Password}"
+                   }));
+                   data.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                   var response = client.PostAsync("https://localhost:7020/api/user/login", data).Result;
+
+                   viewModel.Info = await response.Content.ReadAsStringAsync();
+               }
+           });
         }
     }
 }
