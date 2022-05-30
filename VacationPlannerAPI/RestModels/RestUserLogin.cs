cbedbf8c689ae8639174ec79adcb6bb2a85681e1
Krystian Petek <krystianpetek2@gmail.com>
2022-05-31 00:06:45 +0200

    little change in loginView, add icon and pattern

--- VacationPlannerAPI/RestModels/RestUserLogin.cs
+++ VacationPlannerAPI/RestModels/RestUserLogin.cs
@@ -4,16 +4,8 @@ namespace VacationPlannerAPI.RestModels
 {
     public class RestUserLogin
     {
-        [Required(ErrorMessage = "Username is required"),
-            Display(Name = "Username"),
-            MinLength(6),
-            DataType(DataType.Text)]
         public string Username { get; set; }
 
-        [Required(ErrorMessage = "Password is required"),
-            Display(Name = "Password"),
-            MinLength(8),
-            DataType(DataType.Password)]
         public string Password { get; set; }
 
     }
